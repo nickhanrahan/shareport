8bfc19ef7c5ef31fae02c93240deb7aa9145d45b
nickhanrahan <nicholashanr@gmail.com>
2021-08-24 12:39:28 -0700

    create api routes and create requests

---
+++ shareport/client/src/app/requests.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+const getPortfolios = () => {
+  axios.get('/portfolios')
+    .then((list) => {
+      console.log(list.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getDetails = (id) => {
+  axios.get(`/portfolios/${id}`)
+    .then((details) => {
+      console.log(details.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const postPortfolio = (newPortfolio) => {
+  axios.post('/portfolios', newPortfolio)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = {
+  getPortfolios,
+  getDetails,
+  postPortfolio,
+}
--- shareport/server/index.js
+++ shareport/server/index.js
@@ -8,6 +8,23 @@ app.use(express.json());
 
 app.use(express.static('client/dist'));
 
+app.get('/portfolios', (req, res) => {
+  res.status(200);
+  res.send('get portfolios');
+});
+
+app.get('/portfolios/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200);
+  res.send(`get details for ${id}`);
+});
+
+app.post('/portfolios', (req, res) => {
+  console.log(req.body);
+  res.status(201);
+  res.send('post');
+});
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
