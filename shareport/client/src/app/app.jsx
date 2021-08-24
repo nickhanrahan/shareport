bc9b8ad3aefd69c45901e9235f508314ddf370a5
nickhanrahan <nicholashanr@gmail.com>
2021-08-24 15:00:42 -0700

    create portfolio form open on button click and init css

--- shareport/client/src/app/app.jsx
+++ shareport/client/src/app/app.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PortfolioList from './components/PortfolioList.jsx';
 import CreatePortfolio from './components/CreatePortfolio.jsx';
 import { getPortfolios } from './requests.js';
 
 const App = () => {
+  const [openCreate, setOpenCreate] = useState(false);
 
   return (
-    <>
+    <div className="global">
       <div className="top-bar">
         <div className="shareport-logo">SharePort</div>
+        <button type="button" className="create-btn" onClick={() => setOpenCreate(true)}>Create Portfolio</button>
+        <CreatePortfolio
+          open={openCreate}
+        />
       </div>
       <div className="list-ctr">
         <PortfolioList />
       </div>
-      <CreatePortfolio />
-    </>
+    </div>
   )
 }
 
--- shareport/client/src/app/components/CreatePortfolio.jsx
+++ shareport/client/src/app/components/CreatePortfolio.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './modal.css';
 
-const CreatePortfolio = () => {
+const CreatePortfolio = ({ open }) => {
+  if (!open) return null;
+
   return ReactDOM.createPortal(
     <>
-    <div className="modal-overlay"/>
-    <div className="modal-window">
-      <div className="create-bar">top</div>
-      <div className="create-form-ctr">form</div>
-    </div>
+      <div className="modal-overlay" />
+      <div className="modal-window">
+        <div className="create-bar">top</div>
+        <div className="create-form-ctr">
+          <form className="create-form" />
+        </div>
+      </div>
     </>,
-  document.getElementById('app'),
+    document.getElementById('app'),
   );
-}
+};
 
 export default CreatePortfolio;
