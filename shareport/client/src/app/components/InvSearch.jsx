489ed1c122a4b28ad86d6c9a72934bae3d51c5a2
nickhanrahan <nicholashanr@gmail.com>
2021-08-24 17:30:48 -0700

    create list item for investment search form

--- shareport/client/src/app/components/InvSearch.jsx
+++ shareport/client/src/app/components/InvSearch.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import InvSearchItem from './InvSearchItem.jsx';
 
 const InvSearch = () => {
   const [selected, setSelected] = useState('');
@@ -11,6 +12,8 @@ const InvSearch = () => {
   return (
     <div className="inv-search">
       <input className="inv-search-input" placeholder="Search by Ticker or Company Name..."></input>
+      <ul className="inv-search-list">
+      </ul>
     </div>
   )
 }
---
+++ shareport/client/src/app/components/InvSearchItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const InvSearchItem = () => {
+  return (
+    <button
+      type="button"
+      className="inv-list-item inv-list-item-action"
+    >
+      item
+    </button>
+  )
+}
+
+export default InvSearchItem;
