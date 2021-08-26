11c52f7e5b85ba313027d5baef39f4d856036856
nickhanrahan <nicholashanr@gmail.com>
2021-08-26 00:34:37 -0700

    prepare PortfolioDetails component

---
+++ shareport/client/src/app/components/PortfolioDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './modal-details.css'
+
+const PortfolioDetails = () => {
+  return (
+    <>
+      <div className="modal-overlay" />
+      <div className="modal-window">
+
+      </div>
+    </>
+  );
+}
+
+export default PortfolioDetails;
--- shareport/client/src/app/components/PortfolioItem.jsx
+++ shareport/client/src/app/components/PortfolioItem.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PortfolioDetails from './PortfolioDetails.jsx';
 
 const PortfolioItem = ({ portfolio }) => {
+
+  const [marketValue, setMarketValue] = useState(0);
+  const [dollarGL, setDollarGL] = useState(0);
+  const [percentGL, setPercentGL] = useState(0);
+  const [dayChange, setDayChange] = useState(0);
+  const [isOpen, setOpen] = useState(false);
+
+  const round = (num) => (Math.round(num * 100) / 100);
+
+  useEffect(() => {
+    let value = 0;
+    let newDayChange = 0;
+    portfolio.holdings.forEach((holding) => {
+      value += (holding.quote.price * holding.numberOfShares);
+      newDayChange += (holding.quote.dayChange * holding.numberOfShares);
+    });
+    setMarketValue(round(value));
+    setDollarGL(round(marketValue - portfolio.totalCost));
+    setPercentGL((marketValue / portfolio.totalCost) - 1);
+  }, []);
 
   const dateCreated = (timeStampString) => {
     const dateFormat = new Date(timeStampString);
     const dateString = `${dateFormat.getMonth() + 1}/${dateFormat.getDate()}/${dateFormat.getFullYear()}`;
     return dateString;
-  }
+  };
+
   return (
-    <div className="list-item-ctr">
-      <div className="list-col1">{portfolio.name}</div>
-      <div className="list-col2">{portfolio.username}</div>
-      <div className="list-col3">${portfolio.totalCost}</div>
-      <div className="list-col4"></div>
-      <div className="list-col5"></div>
-      <div className="list-col6"></div>
-      <div className="list-col7"></div>
-      <div className="list-col8">{dateCreated(portfolio.createdAt)}</div>
-    </div>
-  )
-}
+    <>
+      <div className="list-item-ctr">
+        <div className="list-col1" onClick={() => setOpen(true)}>{portfolio.name}</div>
+        <div className="list-col2">{portfolio.username}</div>
+        <div className="list-col3">${portfolio.totalCost}</div>
+        <div className="list-col4">${marketValue}</div>
+        <div className="list-col5">${dollarGL}</div>
+        <div className="list-col6">{percentGL}%</div>
+        <div className="list-col7">${dayChange}</div>
+        <div className="list-col8">{dateCreated(portfolio.createdAt)}</div>
+      </div>
+      <PortfolioDetails
+        open={isOpen}
+        setOpen={setOpen}
+      />
+    </>
+  );
+};
 
 export default PortfolioItem;
