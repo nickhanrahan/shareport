fb9b28bb42cb6307da8f5407fc2e1887fe48b60e
nickhanrahan <nicholashanr@gmail.com>
2021-09-18 17:04:05 -0700

    remote yaml and add to gitignore

--- shareport/server/db/index.js
+++ shareport/server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { MONGOUN, MONGOPW } = require('../../db-config.js');
 
-const mongoURI = `mongodb://${MONGOUN}:${MONGOPW}@localhost:27017`;
+const mongoURI = `mongodb://${MONGOUN}:${MONGOPW}@mongodb:27017/portfolios`;
 
 mongoose.connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true });
 
