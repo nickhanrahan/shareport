c3d0447db5c88fd159c476b6ce8fc035f74cab37
nickhanrahan <nicholashanr@gmail.com>
2021-09-30 09:08:59 -0700

    Update Dockerfile

--- shareport/server/db/index.js
+++ shareport/server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const { MONGOUN, MONGOPW } = require('../../db-config.js');
 
-const mongoURI = `mongodb://${MONGOUN}:${MONGOPW}@mongodb:27017`;
+const mongoURI = `mongodb://${MONGOUN}:${MONGOPW}@mongodb:27017/portfolios`;
 
 mongoose.connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
